6bd9c719953cf6fe8df4533a03fe19a848423173
Carrena <carrena1642@gmail.com>
2022-12-17 22:32:47 +0900

    cc

---
+++ src/Front-end/eum-front-repo/apps/eum-mail/app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>here is eum mail</h1>
+    <EumEditor/>
+  </div>
+</template>
---
+++ src/Front-end/eum-front-repo/apps/eum-portal/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>here is eum-portal</h1>
+    <EumEditor/>
+
+    
+  </div>
+</template>
